96d8409154967d2534c17194af96bfbca9c1c7ef
Joel king yan Hii <jhii@u82z05s02.42kl.edu.my>
2022-06-22 15:33:47 +0800

    added header for minishell

--- main.c
+++ main.c
@@ -6,17 +6,30 @@
 /*   By: jhii <jhii@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 11:45:31 by jhii              #+#    #+#             */
-/*   Updated: 2022/06/21 19:43:08 by jhii             ###   ########.fr       */
+/*   Updated: 2022/06/22 15:27:47 by jhii             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static	void	print_header(void)
+{
+	printf("\n\n");
+	printf(GRNA"█▀▄▀█ ▄█    ▄   ▄█    ▄▄▄▄▄    ▄  █ ▄███▄   █    █     \n");
+	printf(GRNB"█ █ █ ██     █  ██   █     ▀▄ █   █ █▀   ▀  █    █     \n");
+	printf(GRNC"█ ▄ █ ██ ██   █ ██ ▄  ▀▀▀▀▄   ██▀▀█ ██▄▄    █    █     \n");
+	printf(GRND"█   █ ▐█ █ █  █ ▐█  ▀▄▄▄▄▀    █   █ █▄   ▄▀ ███▄ ███▄  \n");
+	printf(GRNE"   █   ▐ █  █ █  ▐               █  ▀███▀       ▀    ▀ \n");
+	printf(GRNF"  ▀      █   ██                 ▀                      \n");
+	printf(WHT"\n\n");
+}
 
 int	main(int argc, char **argv, char **envp)
 {
 	(void)argc;
 	(void)argv;
 	g_filenumber = 0;
+	print_header();
 	minishell(envp);
 	return (0);
 }
--- minishell.h
+++ minishell.h
@@ -6,19 +6,19 @@
 /*   By: jhii <jhii@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 11:45:35 by jhii              #+#    #+#             */
-/*   Updated: 2022/06/22 14:04:08 by jhii             ###   ########.fr       */
+/*   Updated: 2022/06/22 15:32:58 by jhii             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# define RED	"\x1B[31m"
-# define GRN	"\x1B[32m"
-# define YEL	"\x1B[33m"
-# define BLU	"\x1B[34m"
-# define MAG	"\x1B[35m"
-# define CYN	"\x1B[36m"
+# define GRNA	"\x1B[38;5;34m"
+# define GRNB	"\x1B[38;5;34m"
+# define GRNC	"\x1B[38;5;35m"
+# define GRND	"\x1B[38;5;35m"
+# define GRNE	"\x1B[38;5;36m"
+# define GRNF	"\x1B[38;5;36m"
 # define WHT	"\x1B[37m"
 
 # include <stdio.h>
